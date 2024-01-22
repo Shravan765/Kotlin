272cf8a70042d98492c3d7a972917840e4c1470f
Shravan765 <142580349+Shravan765@users.noreply.github.com>
2024-01-22 14:54:55 +0530

    Create helloworld_ifelse_while.kt

---
+++ helloworld_ifelse_while.kt
@@ -0,0 +1,17 @@
+fun main() {
+    println("Hello, world!!!")
+    var a = 1
+    println(a)
+    while (a<10){
+        
+        print(++a)
+        if (a%2 == 1){
+            println(" Odd")
+        }
+        else{
+            println(" Even")
+        }
+        
+    }
+    println("Hello, world!!!")
+}
